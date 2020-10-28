2b2004e5a546755b2deab1e2e554db048b9c5486
Black_J08 <nitin88mehta@gmail.com>
2020-10-29 02:57:51 +0530

    Added Login Page - Created template for login.html - Rendering login.html at '/login' route

--- binance_automation/__init__.py
+++ binance_automation/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
+from pathlib import Path
 
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, root_path=Path(__file__).resolve().parent.parent)
 
     from binance_automation.index import views
     app.register_blueprint(views.index_bp)
@@ -10,6 +11,6 @@ def create_app():
     from binance_automation.login import views
     app.register_blueprint(views.login_bp)
 
-    app.config.from_pyfile('../config.py')
+    app.config.from_pyfile('config.py')
 
     return app
