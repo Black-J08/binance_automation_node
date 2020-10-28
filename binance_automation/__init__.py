5f6bfb30f1e75c2534e719a96f7bdd5b8ee635fe
Black_J08 <nitin88mehta@gmail.com>
2020-10-28 13:59:38 +0530

    Added Login Route

--- binance_automation/__init__.py
+++ binance_automation/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
-from binance_automation.index import views
 
 
 def create_app():
     app = Flask(__name__)
 
+    from binance_automation.index import views
     app.register_blueprint(views.index_bp)
+
+    from binance_automation.login import views
+    app.register_blueprint(views.login_bp)
 
     app.config.from_pyfile('../config.py')
 
